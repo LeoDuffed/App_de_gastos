fbc9784485c9bddfee79bca704febe1d7595c979
LeoDuffed <carlosmroder@gmail.com>
2024-11-01 14:12:31 -0600

    No queda la app
    Estoy haciendo cambios, pero no esta funcionando bien :(

--- App_usando_kivy.py
+++ App_usando_kivy.py
@@ -12,6 +12,8 @@ class app(App):
         # Poner todos los widgets :)
         Window.clearcolor = (1,1,1,1)
 
+        self.lista_precios = []
+
         layout = BoxLayout(orientation='vertical', padding = 20, spacing = 10  )
 
         label_instruccion= Label (text = 'Esta App es para ir anotando tus gastos', font_size = '20sp', color = (1, 0, 0, 1))
@@ -27,9 +29,12 @@ class app(App):
         agregar_boton.bind(on_press = self.agregar_producto)
         layout.add_widget(agregar_boton)
 
-        agregar_boton2 = Button(text = "Gastos totales", size_hint = (0.5, None), height = 100, pos_hint = {"center_x": 0.5}, background_color= (0,1,1,1,))
+        agregar_boton2 = Button(text = "Gastos totales", size_hint = (0.4, None), height = 110, pos_hint = {"center_x": 0.5}, background_color= (0,1,1,1,))
         agregar_boton2.bind (on_press = self.agregar_producto)
         layout.add_widget(agregar_boton2)
+
+        self.resultado_label = Label(text= "")
+        layout.add_widget(self.resultado_label)
 
         scroll_view = ScrollView(size_hint= (1, None), height = 200)
         self.lista_producto = BoxLayout (orientation = 'vertical', size_hint_y= None)
@@ -37,33 +42,27 @@ class app(App):
         scroll_view.add_widget(self.lista_producto)
         layout.add_widget(scroll_view)
 
-        scroll_view = ScrollView(size_hint= (1, None), height = 200) 
-        self.lista_precios = BoxLayout (orientation = 'vertical', size_hint_y = None)
-        self.lista_precios.bind (minimum_height = self.lista_precios.setter('height'))
-        scroll_view.add_widget(self.lista_precios)
-        layout.add_widget(scroll_view)
-
         self.suma_total = 0
 
         return layout
 
     def agregar_producto(self, instance):
-        producto = self.producto_input.text
+        try: 
+            nombre_producto = self.producto_ingreso.text
+            precio = float(self.costo_ingreso.text)
+            self.lista_precios.append(precio)
 
-        costo = float(self.costo_ingreso.text)
-        self.suma_total += costo
-        self.suma_label.text = f"Total: {self.suma_total}"
-        self.lista_precios.add_widget(self.suma_label)
+            etiqueta = Label (text= f"{nombre_producto} - {precio:.2f}", size_hint_y = None, height = 40 )
+            self.lista_producto.add_widget(etiqueta)
 
+            self.producto_ingreso = ""
+            self.costo_ingreso = ""
+            self.resultado_label.texto = "Producto agregado"
+        except ValueError: 
+            self.resultado_label.text = "Ingresa un costo valido"
 
-
-
-
-
-
-
-
-        return layout 
-    
+    def mostrar_total (self, instace): 
+        total = sum(self.lista_precios)
+        self.resultado_label.text = f"Total gastado: {total:.2f}"
 if __name__ == '__main__': 
     app().run()
